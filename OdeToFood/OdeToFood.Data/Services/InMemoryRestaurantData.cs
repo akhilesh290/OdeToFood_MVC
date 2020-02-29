ead805343acae3babe1fb36ecff2beb283ba25cf
Akhilesh Vishwanath Dhuri <40741480+akhilesh297@users.noreply.github.com>
2020-02-29 08:01:28 +0530

    Update

---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,42 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id=1,
+                    Name="Red Ant",
+                    Cuisine=CuisineType.Indian
+                },
+                new Restaurant
+                {
+                    Id=2,
+                    Name="Hotel Spice Chilly",
+                    Cuisine=CuisineType.French
+                },
+                new Restaurant
+                {
+                    Id=3,
+                    Name="Rising",
+                    Cuisine=CuisineType.Italian
+                }
+
+            };
+        }
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
