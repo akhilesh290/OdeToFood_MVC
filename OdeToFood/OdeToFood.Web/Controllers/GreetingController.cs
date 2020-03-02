fefcaa307c6cb15bdef0c81b437711bfa1912c96
Akhilesh Vishwanath Dhuri <40741480+akhilesh297@users.noreply.github.com>
2020-03-02 09:14:09 +0530

    changes

---
+++ OdeToFood/OdeToFood.Web/Controllers/GreetingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Web.Models;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index(string name)
+        {
+            GreetingViewModel model = new GreetingViewModel
+            {
+                Name = name ?? "No data"
+            };
+            return View(model);
+        }
+    }
+}
